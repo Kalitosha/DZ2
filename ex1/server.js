3ec9ea668ce90f4286928ecd31a99161bbe5271a
Kalitosha <burtseva_maria@rambler.ru>
2018-09-15 14:02:59 +0300

    )

--- ex1/server.js
+++ ex1/server.js
@@ -1,31 +1,28 @@
 'use strict';
-
 const http = require('http');
 const process = require('process');
 
-const port = '3000';
-const url = '127.0.0.1';
+const PORT = '3000';
+const URL = '127.0.0.1';
 
-const interval = process.argv[2] || 1000; // кол-во мсек
-const timePeriod = process.argv[3] || 5000;
+process.env.TIME_INTERVAL = process.argv[2] || 1000; // кол-во мсек
+process.env.TIME_PERIOD = process.argv[3] || 5000;
+const interval = process.env.TIME_INTERVAL;
+const timePeriod = process.env.TIME_PERIOD;
 
 http.createServer(function (request, response) {
-
     response.writeHead(200, {
         'Content-Type': 'text/plain',
         'Access-Control-Allow-Origin': '*' // implementation of CORS
     });
-
     if(request.method==='GET'){
             getData( (data)=>{
                 // response.write(data);
                 response.end(data);
             });
     }
-}).listen(port, url);
-
-console.log(`Server running at http://${url}:${port}/`);
-console.log('...');
+}).listen(PORT, URL);
+console.log(`Server running at http://${URL}:${PORT}/ \n ...`);
 
 function getData(callback) {
     let timerId = setInterval(function() { // начать повторы с интервалом = interval
